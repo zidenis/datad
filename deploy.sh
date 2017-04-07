69c7f7a4b3489fb5241b6030aec806eb7f796dfe
zidenis <denisalbuquerque@gmail.com>
2017-04-06 21:40:46 -0300

    structural changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-rm -rf zidenis.github.io
+if [ -d "zidenis.github.io" ]; then
+	echo "removing old structure"
+	for file in ./zidenis.github.io/*; do
+    		[ "$file" = ".git" ] && continue
+		[ "$file" = "CNAME" ] && continue
+    		rm -rf "$file"
+	done
+fi
 hugo -d zidenis.github.io
